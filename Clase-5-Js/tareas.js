d37e402a38efca8cb0c1781f40dbaa4584948000
Zwenger <fzwenger@quilmes.com.ar>
2023-02-21 17:47:38 -0300

    Tarea 5 completa

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -7,6 +7,11 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let counter = 0;
+
+    return function () {
+        return ++counter;
+    }
 }
 
 function cacheFunction(cb) {
@@ -21,6 +26,16 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  let cache = {};
+    return function (argumento){
+      if (cache.hasOwnProperty(argumento)){
+        return cache[argumento];
+      }else{
+        cache[argumento] = cb(argumento);
+        return cache[argumento];
+      }
+    
+    }
 }
 
 // No modifiquen nada debajo de esta linea
